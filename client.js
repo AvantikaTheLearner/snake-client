2bdcff5cd0a7ef8356ea0eb04504f8e48cd2a8ff
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-09-22 19:03:28 -0400

    Connection Module

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    conn.write('Name: Avi');
+  });
+
+  return conn;
+};
+module.exports = {connect};

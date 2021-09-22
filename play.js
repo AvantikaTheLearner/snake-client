a014912c4b8b14954a801840bb15f71d61bbf576
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-09-22 19:58:50 -0400

    Stdin Setup

--- play.js
+++ play.js
@@ -3,3 +3,21 @@ const {connect} = require("./client.js");
 // establishes a connection with the game server
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', (handleUserInput) => {
+    if (handleUserInput === '\u0003') {
+      process.exit();
+    }
+    process.stdout.write('.');
+  });
+  return stdin;
+};
+
+setupInput();

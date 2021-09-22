fec68c28a5c3392fc4f9f42617a994df26b36a83
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-09-22 19:12:48 -0400

    Snake Initials

--- client.js
+++ client.js
@@ -10,6 +10,7 @@ const connect = function() {
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     // code that does something when the connection is first established
+    console.log("Successfully connected to game server");
     conn.write('Name: Avi');
   });
 

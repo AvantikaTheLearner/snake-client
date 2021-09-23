5b2184fe1267f0ac4d304157c691b7b631afc175
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-09-22 20:08:11 -0400

    Input Module

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
-const {connect} = require("./client.js");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 // establishes a connection with the game server
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', (handleUserInput) => {
-    if (handleUserInput === '\u0003') {
-      process.exit();
-    }
-    process.stdout.write('.');
-  });
-  return stdin;
-};
 
 setupInput();
